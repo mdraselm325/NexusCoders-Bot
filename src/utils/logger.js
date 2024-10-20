9a23dabb56834d347154d544b52ba511e4f5c10e
Isaiah-ilom <139964528+Isaiah-ilom@users.noreply.github.com>
2024-10-20 16:58:08 +0100

    Update logger.js

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,31 +1,40 @@
-const winston = require('winston');
+const fs = require('fs-extra');
 const path = require('path');
+const util = require('util');
 
-const logger = winston.createLogger({
-    level: 'info',
-    format: winston.format.combine(
-        winston.format.timestamp(),
-        winston.format.json()
-    ),
-    transports: [
-        new winston.transports.File({ 
-            filename: path.join('logs', 'error.log'), 
-            level: 'error',
-            maxsize: 5242880,
-            maxFiles: 5 
-        }),
-        new winston.transports.File({ 
-            filename: path.join('logs', 'combined.log'),
-            maxsize: 5242880,
-            maxFiles: 5 
-        }),
-        new winston.transports.Console({
-            format: winston.format.combine(
-                winston.format.colorize(),
-                winston.format.simple()
-            )
-        })
-    ]
-});
+class Logger {
+    constructor() {
+        this.logDir = path.join(process.cwd(), 'logs');
+        this.logFile = path.join(this.logDir, `${new Date().toISOString().split('T')[0]}.log`);
+        fs.ensureDirSync(this.logDir);
+    }
 
-module.exports = logger;
+    write(level, message, ...args) {
+        const timestamp = new Date().toISOString();
+        const formattedMessage = args.length ? util.format(message, ...args) : message;
+        const logEntry = `[${timestamp}] [${level}] ${formattedMessage}\n`;
+        
+        console.log(`[${level}] ${formattedMessage}`);
+        fs.appendFileSync(this.logFile, logEntry);
+    }
+
+    info(message, ...args) {
+        this.write('INFO', message, ...args);
+    }
+
+    warn(message, ...args) {
+        this.write('WARN', message, ...args);
+    }
+
+    error(message, ...args) {
+        this.write('ERROR', message, ...args);
+    }
+
+    debug(message, ...args) {
+        if (process.env.DEBUG) {
+            this.write('DEBUG', message, ...args);
+        }
+    }
+}
+
+module.exports = new Logger();
