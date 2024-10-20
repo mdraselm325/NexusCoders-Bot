362bcc072cf14b5afaa699b1847068762373898d
Isaiah-ilom <139964528+Isaiah-ilom@users.noreply.github.com>
2024-10-20 10:38:01 +0100

    Update database.js

--- src/utils/database.js
+++ src/utils/database.js
@@ -4,13 +4,26 @@ const logger = require('./logger');
 
 async function connectToDatabase() {
     try {
-        await mongoose.connect('mongodb+srv://arisonbeckham:arisonbeckham@nexuscoders.yxxbj.mongodb.net/?retryWrites=true&w=majority&appName=NexusCoders', {
+        await mongoose.connect(config.database.uri, {
             useNewUrlParser: true,
-            useUnifiedTopology: true
+            useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 5000,
+            socketTimeoutMS: 45000,
         });
-        logger.info('Connected to MongoDB');
+
+        logger.info('Connected to MongoDB database');
+
+        mongoose.connection.on('error', (err) => {
+            logger.error('MongoDB connection error:', err);
+        });
+
+        mongoose.connection.on('disconnected', () => {
+            logger.warn('MongoDB disconnected. Attempting to reconnect...');
+            setTimeout(connectToDatabase, 5000);
+        });
+
     } catch (error) {
-        logger.error('MongoDB connection failed:', error);
+        logger.error('Database connection failed:', error);
         process.exit(1);
     }
 }
