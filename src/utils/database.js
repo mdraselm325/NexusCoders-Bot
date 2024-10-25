9699a5f48031a725b76d60d0b232ef0ce9f4877e
Isaiah-ilom <139964528+Isaiah-ilom@users.noreply.github.com>
2024-10-25 15:56:06 +0100

    Update database.js

--- src/utils/database.js
+++ src/utils/database.js
@@ -4,7 +4,12 @@ const logger = require('./logger');
 
 async function connectToDatabase() {
     try {
-        await mongoose.connect(config.database.uri, config.database.options);
+        mongoose.set('strictQuery', true);
+        await mongoose.connect(config.database.uri, {
+            ...config.database.options,
+            serverSelectionTimeoutMS: 5000,
+            socketTimeoutMS: 45000,
+        });
         logger.info('Connected to MongoDB');
     } catch (error) {
         logger.error('MongoDB connection error:', error);
@@ -19,6 +24,17 @@ mongoose.connection.on('disconnected', () => {
 
 mongoose.connection.on('error', (err) => {
     logger.error('MongoDB error:', err);
+});
+
+process.on('SIGINT', async () => {
+    try {
+        await mongoose.connection.close();
+        logger.info('MongoDB connection closed through app termination');
+        process.exit(0);
+    } catch (err) {
+        logger.error('Error closing MongoDB connection:', err);
+        process.exit(1);
+    }
 });
 
 module.exports = {
