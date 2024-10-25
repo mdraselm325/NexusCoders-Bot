533d60eb74770c7e49e5c301e56692d53c5f3a6f
Isaiah-ilom <139964528+Isaiah-ilom@users.noreply.github.com>
2024-10-25 14:14:51 +0100

    Update user.js

--- src/models/user.js
+++ src/models/user.js
@@ -6,20 +6,11 @@ const userSchema = new mongoose.Schema({
         required: true,
         unique: true
     },
-    name: String,
-    isAdmin: {
-        type: Boolean,
-        default: false
+    name: {
+        type: String,
+        required: true
     },
-    isBanned: {
-        type: Boolean,
-        default: false
-    },
-    warns: {
-        type: Number,
-        default: 0
-    },
-    xp: {
+    experience: {
         type: Number,
         default: 0
     },
@@ -27,23 +18,112 @@ const userSchema = new mongoose.Schema({
         type: Number,
         default: 1
     },
-    lastCommandUsed: Date,
-    joinedAt: {
+    warns: {
+        type: Number,
+        default: 0
+    },
+    isBanned: {
+        type: Boolean,
+        default: false
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    },
+    lastDaily: {
+        type: Date
+    },
+    balance: {
+        type: Number,
+        default: 0
+    },
+    inventory: [{
+        item: String,
+        quantity: Number
+    }],
+    achievements: [{
+        name: String,
+        unlockedAt: Date
+    }],
+    settings: {
+        notifications: {
+            type: Boolean,
+            default: true
+        },
+        language: {
+            type: String,
+            default: 'en'
+        },
+        prefix: {
+            type: String
+        }
+    },
+    statistics: {
+        commandsUsed: {
+            type: Number,
+            default: 0
+        },
+        messagesReceived: {
+            type: Number,
+            default: 0
+        },
+        messagesSent: {
+            type: Number,
+            default: 0
+        }
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
         type: Date,
         default: Date.now
     }
-}, {
-    timestamps: true
 });
 
-userSchema.methods.addXP = async function(amount) {
-    this.xp += amount;
-    const nextLevel = Math.floor(0.1 * Math.sqrt(this.xp));
-    if (nextLevel > this.level) {
-        this.level = nextLevel;
-    }
+userSchema.pre('save', function(next) {
+    this.updatedAt = new Date();
+    next();
+});
+
+userSchema.methods.addExperience = async function(amount) {
+    this.experience += amount;
+    const levelUp = this.checkLevelUp();
     await this.save();
-    return this.level;
+    return levelUp;
 };
 
-module.exports = mongoose.model('User', userSchema);
+userSchema.methods.checkLevelUp = function() {
+    const nextLevel = Math.floor(0.1 * Math.sqrt(this.experience));
+    if (nextLevel > this.level) {
+        this.level = nextLevel;
+        return true;
+    }
+    return false;
+};
+
+userSchema.methods.warn = async function() {
+    this.warns += 1;
+    await this.save();
+    return this.warns >= config.maxWarns;
+};
+
+userSchema.methods.clearWarns = async function() {
+    this.warns = 0;
+    await this.save();
+};
+
+userSchema.methods.ban = async function() {
+    this.isBanned = true;
+    await this.save();
+};
+
+userSchema.methods.unban = async function() {
+    this.isBanned = false;
+    await this.save();
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
