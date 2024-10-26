058a5e437fd7440bf353e50d0ee26d01b25f453d
NexusCoders-cyber <ilomraphael@gmail.com>
2024-10-26 11:27:12 +0100

    Update messageHandler.js

--- src/handlers/messageHandler.js
+++ src/handlers/messageHandler.js
@@ -2,83 +2,91 @@ const logger = require('../utils/logger');
 const config = require('../config');
 const { executeCommand } = require('./commandHandler');
 
-const messageHandler = {
-    handleMessage: async (sock, message) => {
-        try {
-            if (!message.message) return;
+async function handleMessage(sock, message) {
+    try {
+        if (!message.message) return;
 
-            const messageText = message.message?.conversation || 
-                              message.message?.extendedTextMessage?.text || 
-                              message.message?.imageMessage?.caption ||
-                              message.message?.videoMessage?.caption;
-
-            if (!messageText?.startsWith(config.prefix)) return;
-
-            const [command, ...args] = messageText.slice(config.prefix.length).trim().split(' ');
-            await executeCommand(sock, message, command.toLowerCase(), args);
-        } catch (error) {
-            logger.error('Error in message handler:', error);
+        const messageType = Object.keys(message.message)[0];
+        
+        let messageText;
+        if (messageType === 'conversation') {
+            messageText = message.message.conversation;
+        } else if (messageType === 'extendedTextMessage') {
+            messageText = message.message.extendedTextMessage.text;
+        } else if (messageType === 'imageMessage') {
+            messageText = message.message.imageMessage.caption;
+        } else if (messageType === 'videoMessage') {
+            messageText = message.message.videoMessage.caption;
         }
-    },
 
-    handleGroupParticipantsUpdate: async (sock, update) => {
-        const { id, participants, action } = update;
+        if (!messageText?.startsWith(config.bot.prefix)) return;
 
-        if (!config.features.welcomeMessage) return;
-
-        try {
-            const groupMetadata = await sock.groupMetadata(id);
-
-            for (const participant of participants) {
-                const user = participant.split('@')[0];
-                let message = '';
-
-                switch (action) {
-                    case 'add':
-                        message = `Welcome @${user} to ${groupMetadata.subject}! 🎉`;
-                        break;
-                    case 'remove':
-                        message = `Goodbye @${user}! 👋`;
-                        break;
-                    case 'promote':
-                        message = `@${user} has been promoted to admin! 🎊`;
-                        break;
-                    case 'demote':
-                        message = `@${user} has been demoted! 📉`;
-                        break;
-                }
-
-                if (message) {
-                    await sock.sendMessage(id, {
-                        text: message,
-                        mentions: [participant]
-                    });
-                }
-            }
-        } catch (error) {
-            logger.error('Error in group handler:', error);
-        }
-    },
-
-    handleGroupUpdate: async (sock, update) => {
-        const { id, subject, desc } = update;
-
-        try {
-            if (subject) {
-                await sock.sendMessage(id, {
-                    text: `Group name changed to: ${subject}`
-                });
-            }
-
-            if (desc) {
-                await sock.sendMessage(id, {
-                    text: `Group description updated!`
-                });
-            }
-        } catch (error) {
-            logger.error('Error in group update handler:', error);
-        }
+        const [command, ...args] = messageText.slice(config.bot.prefix.length).trim().split(' ');
+        await executeCommand(sock, message, command.toLowerCase(), args);
+    } catch (error) {
+        logger.error('Error in message handler:', error);
     }
-};
+}
 
-module.exports = messageHandler;
+async function handleGroupParticipantsUpdate(sock, update) {
+    const { id, participants, action } = update;
+
+    try {
+        const groupMetadata = await sock.groupMetadata(id);
+
+        for (const participant of participants) {
+            const user = participant.split('@')[0];
+            let message = '';
+
+            switch (action) {
+                case 'add':
+                    message = `Welcome @${user} to ${groupMetadata.subject}! 🎉`;
+                    break;
+                case 'remove':
+                    message = `Goodbye @${user}! 👋`;
+                    break;
+                case 'promote':
+                    message = `@${user} has been promoted to admin! 🎊`;
+                    break;
+                case 'demote':
+                    message = `@${user} has been demoted! 📉`;
+                    break;
+            }
+
+            if (message) {
+                await sock.sendMessage(id, {
+                    text: message,
+                    mentions: [participant]
+                });
+            }
+        }
+    } catch (error) {
+        logger.error('Error in group handler:', error);
+    }
+}
+
+async function handleGroupUpdate(sock, update) {
+    const { id, subject, desc } = update;
+
+    try {
+        if (subject) {
+            await sock.sendMessage(id, {
+                text: `Group name changed to: ${subject}`
+            });
+        }
+
+        if (desc) {
+            await sock.sendMessage(id, {
+                text: `Group description updated!`
+            });
+        }
+    } catch (error) {
+        logger.error('Error in group update handler:', error);
+    }
+}
+
+module.exports = {
+    handleMessage,
+    handleGroupParticipantsUpdate,
+    handleGroupUpdate
+};
